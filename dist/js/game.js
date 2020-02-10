2f4e0ed5671958622d0204d099ea88df41536d39
Felipe Salas Casasola <felipe.salas@possible.com>
2020-02-10 14:53:58 -0600

    Initial Commit.

---
+++ dist/js/game.js
@@ -0,0 +1,51 @@
+/*! project-name v0.0.1 | (c) 2020 YOUR NAME | MIT License | http://link-to-your-git-repo.com */
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const courages = document.querySelectorAll('.courage');
+let lastHole;
+let timeUp = true;
+let score = 0;
+
+const randomTime = (min , max) => {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+const randomHole = (holes) => {
+  let id = Math.floor(Math.random()* holes.length);
+  let hole = holes[id];
+  if(hole === lastHole) {
+    randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
+
+const peep = () => {
+  const time = randomTime(500, 1500);
+  const hole = randomHole(holes);
+  hole.classList.add('up');
+  setTimeout(() => {
+    hole.classList.remove('up');
+    if(!timeUp) peep();
+  }, time);
+
+}
+
+const startGame = () => {
+  scoreBoard.textContent = "score: 0";
+  timeUp = false;
+  score = 0;
+  peep();
+  setTimeout( ()=> timeUp = true, 10000 );
+}
+
+const bonk = (e) => {
+  if(!e.isTrusted) return;
+  score += 1;
+  console.log(e);
+  (e.target).offsetParent.classList.remove('up');
+  scoreBoard.textContent = "score: " + score;
+}
+
+courages.forEach(courage => courage.addEventListener('click', bonk));
+//
